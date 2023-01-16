59fc436ddf02b00a73a205d1c308b7188c75400b
M. H. Nahib <mhnahib@gmail.com>
2023-01-16 19:16:38 +0600

    removed comment

--- src/controllers/home.controller.js
+++ src/controllers/home.controller.js
@@ -2,7 +2,6 @@ const { StatusCodes } = require("http-status-codes/build/cjs/status-codes");
 const { response } = require("../utils/response");
 
 const homeController = async (req, res) => {
-  console.log("hi");
   return response(
     res,
     StatusCodes.ACCEPTED,
