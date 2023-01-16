3b3af9998e873204a02fd42740590443595cf665
M. H. Nahib <mhnahib@gmail.com>
2023-01-16 18:56:41 +0600

    added response template

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const express = require("express");
+const { StatusCodes } = require("http-status-codes/build/cjs/status-codes");
+const { response } = require("./src/utils/response");
 
 const app = express();
 
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send({ say: "hello" });
+  return response(
+    res,
+    StatusCodes.ACCEPTED,
+    true,
+    { say: "hello world" },
+    "Accepted"
+  );
 });
 
 app.listen(port, () => console.log(`on port ${port}`));
